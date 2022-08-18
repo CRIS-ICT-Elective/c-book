66ca11c278e36b2815cd3d68f7d02d0f95c4f534
Teacher Isaac <isaac@cris.ac.th>
2022-08-18 10:47:14 +0700

    created assignment

---
+++ ex-1-2/print-escaped.c
@@ -0,0 +1,12 @@
+/*
+Write a C program that prints the following output. Indented lines
+should lead with a <TAB> character.
+
+Home Directory Paths
+====================
+    *nix:
+    /home/user
+
+    Windows:
+    C:\Users
+*/
