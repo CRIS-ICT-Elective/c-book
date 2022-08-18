716a4c00660ac3416f12a9adc1130d464ae51dae
cris-ict-pheem <pheem@cris.ac.th>
2022-08-18 11:43:21 +0700

    Finished exercise 1-2

--- ex-1-2/print-escaped.c
+++ ex-1-2/print-escaped.c
@@ -10,3 +10,15 @@ Home Directory Paths
     Windows:
     C:\Users
 */
+
+#include <stdio.h>
+
+int main() {
+	printf("\nHome Directory Paths\n");
+	printf("====================\n");
+	printf("\t*nix:\n");
+	printf("\t/home/user\n\n");
+	printf("\tWin\dows:\n");
+	printf("\tC:\\Users\n\n");
+	return 0;
+}
